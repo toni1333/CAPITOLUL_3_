13070ccad68c17d834e7ea9856ec52a5bda26966
toni1333 <antoniebohak@yahoo.com>
2020-06-16 19:22:49 +0300

    `

--- 3.1 Calcularea pretului unei masini/main.cpp
+++ 3.1 Calcularea pretului unei masini/main.cpp
@@ -6,28 +6,25 @@ int main()
 {
     unsigned int pret= 7000;
        cout<<"DORITI SA ADAUGATI CLIMA? 500EURO (d /n)"<<endl;
-         char raspuns1 ='n';
-           cin>>raspuns1;
+         char raspuns ='n';
+           cin>>raspuns;
 
-                 if ((raspuns1 == 'd') || (raspuns1 == 'D'))
+                 if ((raspuns == 'd') || (raspuns == 'D'))
                      pret+=500;
 
          cout<<"VRETI SA ADAUGATI TRACTIUNE 4X4? 1000EURO (d /n)"<<endl;
-           char raspuns2='n';
-             cin>>raspuns2;
-                   if ((raspuns2 == 'd') || (raspuns2 == 'D'))
+             cin>>raspuns;
+                   if ((raspuns == 'd') || (raspuns == 'D'))
                        pret +=1000;
 
                 cout<<"VRETI SCAUNE DE PIELE? 250EURO (d /n)"<<endl;
-                   char raspuns3='n';
-                     cin>>raspuns3;
-                        if ((raspuns3 == 'd') || (raspuns3 == 'D'))
+                     cin>>raspuns;
+                        if ((raspuns == 'd') || (raspuns == 'D'))
                             pret +=250;
 
                     cout<<"VRETI SA ADAUGATI 8 BOXE? 125EURO (d /n)"<<endl;
-                       char raspuns4='n';
-                         cin>>raspuns4;
-                          if ((raspuns4 == 'd') || (raspuns4 == 'D'))
+                         cin>>raspuns;
+                          if ((raspuns == 'd') || (raspuns == 'D'))
                               pret +=125;
 
      cout<<"PRET FINAL! "<<pret;
